6311cafd1de2eb11714fd4a99f4118b0752624e1
Jonas Kello <jonas.kello@divid.se>
2017-07-10 00:52:39 +0200

    Add example

---
+++ example/webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  context: path.resolve(__dirname, 'src'),
+  devtool: 'sourcemap',
+  entry: './index',
+  output: {
+    path: path.join(__dirname, 'output'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /^node_modules/,
+        loader: "ts-loader",
+        options: {
+          configFileName: "./example/tsconfig.json",
+        }
+      },
+    ]
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+    // plugins: [
+    //   new atl.TsConfigPathsPlugin({ configFileName: "./src/client/tsconfig.json" })
+    // ]
+  },
+};
