828de1869013bdc03d1a9c705718ae3a9cde3ec6
Jonas Kello <jonas.kello@divid.se>
2018-03-04 10:48:33 +0100

    webpack4

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -2,6 +2,7 @@ const path = require("path");
 const TsconfigPathsPlugin = require("../index");
 
 module.exports = {
+  mode: "development",
   context: path.resolve(__dirname, "src"),
   entry: "./index",
   output: {
